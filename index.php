6b2e9978452de2694ded4880ff83de1d9887fdf5
Yael Palomo <39205023+YAPC1404@users.noreply.github.com>
2023-10-03 13:30:44 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,106 @@
+<!--Yael Palomo Cardenas-->
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<link rel="stylesheet" href="estilos.css" type="text/css" />
+		<title>Sala Gamer | Registro</title>
+	</head>
+	<body>
+		<div id="contenedor">
+			<div id="cabecera">
+				<img src="imagenes/logo borregos.png" alt="logo borregos gaming" class="logo">
+				<img alt="frase motivadora lol" class="frase">
+			</div>
+			<div id="avisos">
+				<div id="lblavisos">
+					<center>
+						<br><h2>¡AVISOS!</h2>
+					</center>
+				</div>
+				<center>
+					<font color="white">
+						<br><br><h3>1. Horarios de cierre: Lunes, Martes y Viernes de 2 a 4 p.m.</h3>
+					</font>
+					<font color="aqua">
+						<h4>Por clases life</h4>
+					</font><br>
+					<font color="white">
+						<h3>Jueves de 2 a 8 p.m.</h3>
+					</font>		
+					<font color="aqua">
+						<h4>Por clases life y entrenamiento</h4>
+					</font><br>	
+				</center>
+				<div id="lblavisos">
+					<center>
+						<br><h2>REGLAMENTO</h2>
+					</center>
+				</div>	
+			</div>
+			<div style="width: 100%; height: 90px; background-color: white; margin-top: -50px;">
+				
+			</div>
+			<div id="formulario">
+				<br><br></b><center>
+					<h2 class="lblInstruccion">Registra los siguientes datos para ingresar</h2><br><br>
+					<div id="datos_form">
+						<div id="select-equipo-div">
+							<h4 class="lblSelect">Selecciona pc o consola</h4>
+							<select name="equipo" class="combobox">
+								<option value="----">----</option>
+								<option value="PC01">PC01</option>
+								<option value="PC02">PC02</option>
+								<option value="PC03">PC03</option>
+								<option value="PC04">PC04</option>							
+								<option value="PC06">PC06</option>
+								<option value="PC07">PC07</option>
+								<option value="PC08">PC08</option>
+								<option value="PC09">PC09</option>
+								<option value="PC10">PC10</option>
+								<option value="PC11">PC11</option>
+								<option value="PC12">PC12</option>
+								<option value="PC13">PC13</option>
+								<option value="PC14">PC14</option>
+								<option value="PC15">PC15</option>
+								<option value="PC16">PC16</option>
+								<option value="PC17">PC17</option>
+								<option value="PC18">PC18</option>
+								<option value="PC19">PC19</option>
+								<option value="PC20">PC20</option>
+								<option value="SWITCH">SWITCH</option>
+								<option value="XBOX">XBOX</option>
+							</select>
+						</div>	
+						<div id="select-vgame-div">
+							<h4 class="lblvgame">Selecciona un videojuego</h4>
+							<select name="equipo" class="combobox">
+								<option value="----">----</option>
+								<option value="VALORANT">VALORANT</option>
+								<option value="LOL">LOL</option>
+								<option value="FORTNITE">FORTNITE</option>
+								<option value="ROCKET LEAGUE">ROCKET LEAGUE</option>							
+								<option value="OVERWATCH">OVERWATCH</option>
+								<option value="SMASH">SMASH</option>
+								<option value="MARIO KART">MARIO KART</option>
+								<option value="FIFA">FIFA</option>
+								<option value="MADDEN">MADDEN</option>
+								<option value="F1">F1</option>
+								<option value="FORZA">FORZA</option>
+								<option value="OTROS">OTRO</option>
+							</select>
+						</div>
+						<div id="matricula-div">
+							<h4 class="lblvgame">Ingresa matricula</h4>
+							<input type="text" name="matricula" placeholder="matricula" class="txtmatricula">
+						</div>
+					</div>
+					<div id="espacios">
+							
+					</div>
+				</center>
+			</div>
+		</div>
+		
+	</body>
+</html>
